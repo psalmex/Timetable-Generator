f87337c77a4b8617c9a74bc30b84eefde1c71e3c
psalmex <samexbaba4all@gmail.com>
2021-03-20 17:27:48 +0100

    forms

--- script.js
+++ script.js
@@ -1,1 +1,23 @@
-const getElementById(kali);
+// document.getElementById('login').style.display='block'"
+// const getElementById(kali);
+
+function check(form) {
+
+    if (form.userid.value == "samuel" && form.pwd.value == "samuel") {
+        return true;
+    }
+    else {
+        alert("Error Password or Username")
+        return false;
+    }
+}
+
+// Get the modal
+var modal = document.getElementById('id01');
+
+// When the user clicks anywhere outside of the modal, close it
+window.onclick = function(event) {
+    if (event.target == modal) {
+        modal.style.display = "none";
+    }
+}
